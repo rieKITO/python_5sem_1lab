984c8fce5a2505efc80090237ac1356d85d7771f
Alexander Potemkin <potemkin_av7@mail.ru>
2023-09-21 02:30:57 +0300

    execution_time -> large_factorial

--- tasks/LargeFactorial.py
+++ tasks/LargeFactorial.py
@@ -1,10 +1,22 @@
 import threading
 import sys
+import time
 
 # TASKS
 from tasks.factorial import factorial
 from tasks.InputHandling import correct_input_int
 
+def execution_time(function):
+    def wrapper(*args, **kwargs):
+        startTime = time.time()
+        result = function(*args, **kwargs)
+        endTime = time.time()
+        executionTime = format((endTime - startTime) * 1000, '.100f')
+        print(f"{function.__name__} was completed in {executionTime}ms\n")
+        return result
+    return wrapper
+
+@execution_time
 def calculate_factorial(number, threadsCount):
 
     def partial_factorial(start, end):
--- tasks/add.py
+++ tasks/add.py
@@ -3,17 +3,6 @@ import time
 # TASKS
 from tasks.InputHandling import correct_input_float
 
-def execution_time(function):
-    def wrapper(*args, **kwargs):
-        startTime = time.time()
-        result = function(*args, **kwargs)
-        endTime = time.time()
-        executionTime = endTime - startTime
-        print(f"{function.__name__} was completed in {executionTime}s\n")
-        return result
-    return wrapper
-
-@execution_time
 def add(x):
     def inner(y):
         return x + y
